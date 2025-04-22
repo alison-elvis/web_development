7aae2adfb224bd1d6cebb5e2e4e2d93107504808
alison-elvis <alisonelvis3@gmail.com>
2025-04-22 12:22:17 -0300

    Update script.js
    Melhorias no formato do resultado

--- js/script.js
+++ js/script.js
@@ -2,29 +2,30 @@ document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault();
     const n1 = Number(document.querySelector("#n1").value);
     const n2 = Number(document.querySelector("#n2").value);
-    const operatior = document.querySelector("#operations").value;
+    const operator = document.querySelector("#operations").value;
     const result = document.querySelector(".result");
     let calculate;
 
-    switch (operatior) {
+    switch (operator) {
         case "+":
             calculate = n1 + n2;
-            result.innerText = `${n1} + ${n2} = ${calculate}`;
             break;
         case "-":
             calculate = n1 - n2;
-            result.innerText = `${n1} - ${n2} = ${calculate}`;
             break;
         case "*":
             calculate = n1 * n2;
-            result.innerText = `${n1} * ${n2} = ${calculate}`;
             break;
         case "/":
             calculate = n1 / n2;
-            result.innerText = `${n1} / ${n2} = ${calculate}`;
             break;
         default:
             result.innerText = "Operação não reconhecida!";
     }
+    const formattedValue = new Intl.NumberFormat("pt-BR", {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2
+    }).format(calculate);
+    result.innerText = `${n1} ${operator} ${n2} = ${formattedValue}`;
     document.querySelectorAll("#n1, #n2").forEach(input => input.value = "");
 });
